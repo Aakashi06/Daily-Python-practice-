28cc48230b05bf991395477293ff585f340c8ad7
Aakashi <jaiswalaakashi123@gmail.com>
2026-02-09 17:29:01 +0530

    initial commit

---
+++ numpy/indexing.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+mat = np.array([[1,2,3],[4,5,6]])
+
+print(mat[0,1]) 
+print(mat[1,2])
---
+++ numpy/slicing.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr=np.array([10,20,30,40,50])
+print("Slicing of 1D array:")
+print(arr[1:4])
+
+
+
+# 2D Slicing
+mat = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print("Slicing of 2D array:")
+print(mat[0:2,0:2])  # Slicing the first two rows and first two columns
