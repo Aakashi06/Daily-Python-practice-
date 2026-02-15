afbab04d53376f5e911db6067c3c61c4cae8cc9e
Aakashi <jaiswalaakashi123@gmail.com>
2026-02-15 19:33:25 +0530

    initial commit

---
+++ numpy/Random Module/normaldist.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+print(np.random.randn(3,2)) # generates a 3x2 array of samples from the standard normal distribution (mean=0, std=1)
---
+++ numpy/Random Module/settingseed.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+np.random.seed(42) # seed fixes an starting point for generating random numbers, so that the same sequence of random numbers is produced every time the code is run
+print(np.random.randint(1, 10, 5)) #it ensures to give same random number everytime
